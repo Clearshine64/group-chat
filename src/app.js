56bcb5a5e2b734390d226490dd8c0097c750bf6b
vitalikvaraksa <84393466+vitalikvaraksa@users.noreply.github.com>
2021-05-21 20:18:38 +0300

    https module is added for webrtc requirement

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 let express = require( 'express' );
-let app = express();
-let server = require( 'http' ).Server( app );
+let app = express(); 
+
+//http server
+//let server = require( 'http' ).Server( app ); 
+
+//https server
+let fs = require('fs');
+let privateKey  = fs.readFileSync('key.pem', 'utf8');
+let certificate = fs.readFileSync('cert.pem', 'utf8');
+let credentials = {key: privateKey, cert: certificate};
+let server = require('https').createServer(credentials, app);
+
 let io = require( 'socket.io' )( server );
 let stream = require( './ws/stream' );
 let path = require( 'path' );
@@ -12,7 +22,6 @@ app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 app.get( '/', ( req, res ) => {
     res.sendFile( __dirname + '/index.html' );
 } );
-
 
 io.of( '/stream' ).on( 'connection', stream );
 
