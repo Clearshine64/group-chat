a99dd21a4ffb5366bfc072226122738fd605e0f0
vitalikvaraksa <84393466+vitalikvaraksa@users.noreply.github.com>
2021-05-26 08:38:30 +0300

    first page is routed to login page

--- src/app.js
+++ src/app.js
@@ -16,9 +16,13 @@ let favicon = require( 'serve-favicon' );
 app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
 app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 
-app.get( '/', ( req, res ) => {
-    res.sendFile( __dirname + '/index.html' );
+app.get( '/create', ( req, res ) => {
+   res.sendFile( __dirname + '/index.html' );
 } );
+
+app.get( '/', ( req, res ) => {
+    res.sendFile( __dirname + '/login.html' );
+ } );
 
 io.of( '/stream' ).on( 'connection', stream );
 
